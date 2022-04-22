2db3e330d1c70145ff0fc15a1c4d97c91ce97a3e
markkong30 <markkong30@gmail.com>
2022-04-22 12:23:48 +0800

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./containers/Login";
 import Home from "./containers/Home";
 import "./App.css";
 
 function App() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const User = localStorage.getItem("user") !== "undefined" ? JSON.parse(localStorage.getItem("user")) : localStorage.clear();
+
+		if (!User) navigate("/login");
+	}, []);
+
 	return (
 		<Routes>
 			<Route path="login" element={<Login />}></Route>
