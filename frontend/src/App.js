47f1e30555d2fbf4ac4a1f927a072ac9463318a7
markkong30 <markkong30@gmail.com>
2022-04-22 12:32:20 +0800

    bug fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
 	return (
 		<Routes>
-			<Route path="login" element={<Login />}></Route>
+			<Route path="/login" element={<Login />}></Route>
 			<Route path="/*" element={<Home />}></Route>
 		</Routes>
 	);
